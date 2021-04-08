34f3f7fa6c622e4c18a49ab131cf87cfd76647a8
yi liu <yiliu81@hotmail.com>
2021-04-08 10:31:01 -0400

    3rd

--- util/personsearch.php
+++ util/personsearch.php
@@ -3,23 +3,34 @@
 	require_once $dir."\sqlsts.php";
 	require_once $dir.'\dbops.php';
 	if(isset($_GET['PersonFN']))
-	{
-		$partName=$_GET['PersonFN'];
-		$field="PersonFN";
-		}
+		{
+			$partName=$_GET['PersonFN'];
+			$field="PersonFN";
+			}
 	else if(isset($_GET['PersonLN']))
+		{
+			$partName=$_GET['PersonLN'];
+			$field="PersonLN";
+		}
+	$query="SELECT DISTINCT $field FROM tblpeople 
+		         WHERE $field like \"$partName%\" 
+		         ORDER BY $field";
+	if(isset($_GET['need']))
 	{
-		$partName=$_GET['PersonLN'];
 		$field="PersonLN";
+		$fieldFN="PersonFN";
+		$query="SELECT DISTINCT $field FROM tblpeople 
+		         WHERE $fieldFN like \"$partName%\" 
+		         ORDER BY $field";
 	}
 	$conn=connectDB();
 	$names=array();
-	$query="SELECT DISTINCT $field FROM tblpeople WHERE $field like 
-	    \"$partName%\" ORDER BY $field";
+	
 	$arr=getResultFromTable($conn,$query);
 	foreach($arr as $record)
 	{
 	 array_push($names,$record["$field"]);
 	}
+	$conn->close();
 	echo json_encode($names);
 ?>
